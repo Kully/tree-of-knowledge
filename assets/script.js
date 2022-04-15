79f3cb8371da2bf210abb51b3b7e5a8340d2c9be
Adam Kulidjian <adam.kulidjian@gmail.com>
2022-04-15 15:55:21 -0500

    update the movement of the creature

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,25 @@
-const FPS = 60;
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
+const FPS = 60;
 
-const STROKE_COLOR = "#ddd";
+const STROKE_COLOR = "#FFFFFF22";
 const CANVAS_COLOR = "#222";
-const NODE_COLORS = {
-	default: "#555",
-	highlighted: "#FFE400",
-	invalid: "#FA5560",
-}
+const NODE_COLOR = "#555";
 
-const STATE = {
-	is_lasso: false,
-	is_dragging: false,
-	dragging_node_idx: -1,
-	lasso_start: -1,
-	lasso_end: -1,
-}
+const NODES = [];
+// const NODE_COUNT = 200;
+const NODE_COUNT = 100;
+const NODAL_INV_DENSITY = 0;
+const NETWORK_RADIUS = CANVAS_HEIGHT / 2;
+const MIN_RADIUS = 1;
+const MAX_RADIUS = 1;
 
-
-const NODES = [
-	{
-		x: 230,
-		y: 450,
-		radius: 10,
-		name: "",
-	},
-	{
-		x: 110,
-		y: 90,
-		radius: 10,
-		name: "",
-	},
-	{
-		x: 20,
-		y: 80,
-		radius: 10,
-		name: "",
-	},
-];
-
-
-/*
-	Functions
-*/
 
 function clear(canvas)
 {
 	ctx.fillStyle = CANVAS_COLOR;
 	ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
-
-function draw_lines_between_nodes(NODES)
-	{
-		for(let i=0; i < NODES.length; i += 1)
-		for(let k=0; k < NODES.length; k += 1)
-		{
-			if(i > k)
-			{
-				ctx.beginPath();
-				ctx.moveTo(
-					NODES[i]["x"] * CANVAS_WIDTH,
-					NODES[i]["y"] * CANVAS_HEIGHT,
-				);
-				ctx.lineTo(
-					NODES[k]["x"] * CANVAS_WIDTH,
-					NODES[k]["y"] * CANVAS_HEIGHT,
-				);
-				ctx.strokeStyle = STROKE_COLOR;
-				ctx.stroke();
-			}
-		}
-	}
-
-
-function draw_node(ctx, node)
-{
-	ctx.beginPath();
- 	ctx.arc(
- 		node["x"],
- 		node["y"],
- 		node["radius"],
- 		0,
- 		2 * Math.PI,
- 		false,
- 	);
- 	
-
- 	// select the node color based on the node's state
- 	let node_color;
- 	node_color = NODE_COLORS["default"];
-
- 	if(STATE["dragging_node_idx"] > -1 && NODES[STATE["dragging_node_idx"]] === node)
- 	{
- 		node_color = NODE_COLORS["highlighted"];
- 	}
- 	else
- 	{
- 		node_color = NODE_COLORS["default"];
-	}
-
-	ctx.fillStyle = node_color;
-	ctx.fill();
-}
-
 
 function is_inside_box(x, y, box_x0, box_x1, box_y0, box_y1)
 {
@@ -112,98 +28,113 @@ function is_inside_box(x, y, box_x0, box_x1, box_y0, box_y1)
 	return false;
 }
 
-const lasso_button = document.getElementById("lasso-button");
+function generate_nodes()
+{
+	for(let i=0; i<NODE_COUNT; i+=1)
+	{
+		let r = NETWORK_RADIUS;
+		let theta = Math.random() * 2 * Math.PI;
+		let dist_to_circle = Math.log(Math.random() * Math.E);
+
+		let x = CANVAS_WIDTH/2 + Math.random() * (r * Math.cos(theta));
+		let y = CANVAS_HEIGHT/2 + Math.random() * (r * Math.sin(theta));
+
+		let radius = Math.floor(
+			MIN_RADIUS + (MAX_RADIUS - MIN_RADIUS) * Math.abs(dist_to_circle)
+		);
+
+		// deltaX and deltaY determine how the nodes animate in time
+		let node = {
+			x: x,
+			y: y,
+			color: NODE_COLOR,
+			radius: radius,
+			deltaX: 0,
+			deltaY: 0,
+		}
+		NODES.push(node);
+	}
+}
+
+function draw_node_to_canvas(ctx, x, y, color, radius)
+{
+	ctx.beginPath();
+ 	ctx.arc(
+ 		x,
+ 		y,
+ 		radius,
+ 		0,
+ 		2 * Math.PI,
+ 		false,
+ 	);
+	ctx.fillStyle = NODE_COLOR;
+	ctx.fill();
+}
+
+function draw_all_nodes(nodes)
+{
+	for(n of nodes)
+	{
+		draw_node_to_canvas(
+			ctx,
+			n["x"] + n["deltaX"],
+			n["y"] + n["deltaY"],
+			n["color"],
+			n["radius"],
+		);
+	}
+}
+
+function draw_network_lines()
+{
+	for(let i=0; i<NODES.length; i+=1)
+	for(let j=i; j<NODES.length; j+=1)
+	{
+		if(Math.random() > NODAL_INV_DENSITY)
+		{
+			let node_i = NODES[i];
+			let node_j = NODES[j];
+
+			ctx.strokeStyle = STROKE_COLOR;
+			ctx.beginPath();
+			ctx.moveTo(
+				node_i["x"] + node_i["deltaX"],
+				node_i["y"] + node_i["deltaY"]
+			);
+			ctx.lineTo(
+				node_j["x"] + node_j["deltaX"],
+				node_j["y"] + node_j["deltaY"]
+			);
+			ctx.stroke();
+		}
+	}
+}
+
+function perturb_nodes(nodes, counter)
+{
+	for(n of nodes)
+	{
+		n["deltaX"] = 4*Math.cos( 0.04 * counter + (2*Math.random()-1) ) +      1*Math.tan(0.00004*counter + (2*Math.random()-1) );
+		n["deltaY"] = 2*Math.sin(1 + Math.PI/90 * counter + (2*Math.random()-1) );
+	}
+}
+
+
+// setup
 const canvas = document.getElementById("app-canvas");
 const ctx = canvas.getContext("2d");
 canvas.height = CANVAS_HEIGHT;
 canvas.width = CANVAS_WIDTH;
 
-clear(canvas);
-for(node of NODES)
-	draw_node(ctx, node);
+generate_nodes();
 
+// loop
+let counter = 0
+window.setInterval(e => {
+	clear(canvas);
+	draw_all_nodes(NODES);
+	perturb_nodes(NODES, counter);
+	draw_network_lines();
+	counter += 1;
+}, 1000 / FPS);
 
-
-/*
-	Event Handlers
-*/
-
-canvas.addEventListener("mousedown", function(e) {
-
-	if(STATE["is_lasso"] === false)
-	{
-		for(let idx=0; idx<NODES.length; idx+=1)
-		{
-			let box_x0 = NODES[idx]["x"] - NODES[idx]["radius"];
-			let box_x1 = NODES[idx]["x"] + NODES[idx]["radius"];
-			let box_y0 = NODES[idx]["y"] - NODES[idx]["radius"];
-			let box_y1 = NODES[idx]["y"] + NODES[idx]["radius"];
-
-			if(
-				is_inside_box(e.offsetX, e.offsetY, box_x0, box_x1, box_y0, box_y1)
-			)
-			{
-				draw_node(ctx, NODES[idx]);
-				STATE["dragging_node_idx"] = idx;
-			}
-		}
-	}
-	else
-	{
-		STATE["lasso_start"] = [e.offsetX, e.offsetY];
-	}
-})
-
-canvas.addEventListener("mousemove", function(e) {
-	// move node if we just clicked on one
-	if(STATE["is_lasso"] === false && STATE["dragging_node_idx"] > -1)
-	{
-		clear(canvas);
-
-		// update new location of moved node
-		let node_idx = STATE["dragging_node_idx"];
-		NODES[node_idx]["x"] = e.offsetX;
-		NODES[node_idx]["y"] = e.offsetY;
-
-		// redraw the nodes to screen
-		for(node of NODES)
-			draw_node(ctx, node);
-	}
-	else
-	if(STATE["is_lasso"] === true && STATE["lasso_start"] !== -1)
-	{
-		clear(canvas);
-
-		// update the lasso coordinates
-		STATE["lasso_end"] = [e.offsetX, e.offsetY];
-
-		// redraw the nodes as they were
-		for(node of NODES)
-			draw_node(ctx, node);
-
-		ctx.fillStyle = NODE_COLORS["highlighted"] + "11";
-		ctx.strokeStyle = "dotted";
-		ctx.fillRect(
-			STATE["lasso_start"][0],
-			STATE["lasso_start"][1],
-			STATE["lasso_end"][0] - STATE["lasso_start"][0],
-			STATE["lasso_end"][1] - STATE["lasso_start"][1],
-		);
-	}
-})
-
-canvas.addEventListener("mouseup", function(e) {
-	STATE["dragging_node_idx"] = -1;
-	
-	// disable lasso mode
-	STATE["is_lasso"] = false;
-	canvas.style.cursor = "default";
-})
-
-
-lasso_button.addEventListener("click", function(e) {
-	canvas.style.cursor = "crosshair";
-	STATE["is_lasso"] = true;
-	STATE["lasso_start"] = -1;
-	STATE["lasso_end"] = -1;
-})
